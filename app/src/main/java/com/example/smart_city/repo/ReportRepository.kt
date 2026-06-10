f0726fc7e9251d2d8b64e75bfa6f0c3007ee55cd
ghimirepurnima09 <ghimirepurnima570@gmail.com>
2026-06-10 17:16:04 +0545

    create new file

---
+++ app/src/main/java/com/example/smart_city/repo/ReportRepository.kt
@@ -0,0 +1,13 @@
+package com.example.smart_city
+
+import com.google.firebase.database.FirebaseDatabase
+
+class ReportRepository {
+    private val database = FirebaseDatabase.getInstance().getReference("Complaints")
+
+    fun submitReport(report: ReportModel, onComplete: (Boolean) -> Unit) {
+        val id = database.push().key ?: return
+        database.child(id).setValue(report)
+            .addOnCompleteListener { onComplete(it.isSuccessful) }
+    }
+}
